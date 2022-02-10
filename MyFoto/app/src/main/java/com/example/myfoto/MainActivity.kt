d628bd6a32b09286d9e611469b62efafda6dc3e5
leonskyd <leonsky@inbox.ru>
2022-02-10 16:36:31 +0200

    Already done according lesson 1

--- MyFoto/app/src/main/java/com/example/myfoto/MainActivity.kt
+++ MyFoto/app/src/main/java/com/example/myfoto/MainActivity.kt
@@ -2,17 +2,22 @@ package com.example.myfoto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.myfoto.ui.main.MainFragment
+import com.example.myfoto.navigation.ViewPagerAdapter
+import com.example.myfoto.ui.main.DailyPictureFragment
+import kotlinx.android.synthetic.main.activity_api.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
+        setContentView(R.layout.activity_api)
+       /* if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
+                    .replace(R.id.container, DailyPictureFragment.newInstance())
                     .commitNow()
-        }
+        }*/
+        view_pager.adapter = ViewPagerAdapter(
+            supportFragmentManager)
     }
 }
---
+++ MyFoto/app/src/main/java/com/example/myfoto/ui/main/PictureOfTheDayData.kt
@@ -0,0 +1,9 @@
+package com.example.myfoto.ui.main
+
+sealed class PictureOfTheDayData {
+    data class Success(val serverResponseData: PODServerResponseData)
+        : PictureOfTheDayData()
+    data class Error(val error: Throwable) : PictureOfTheDayData()
+    data class Loading(val progress: Int?) : PictureOfTheDayData()
+}
+

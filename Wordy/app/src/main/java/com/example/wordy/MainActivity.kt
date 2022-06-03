9b796db091c12c1a780957dcdf5720aa1c84545a
leonskyd <leonsky@inbox.ru>
2022-06-03 22:58:42 +0300

    ДЗ к уроку 1

--- Wordy/app/src/main/java/com/example/wordy/MainActivity.kt
+++ Wordy/app/src/main/java/com/example/wordy/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.wordy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.wordy.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AppView {
+
+    private lateinit var binding: ActivityMainBinding
+    private var presenter: MainPresenter? = null
+    private val ERROR_MESSAGE = "There is no such a word in the dictionary !!!"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        presenter = MainPresenter()
+        presenter?.onAttach(this)
+
+        binding.searchFab.setOnClickListener {
+            val word = binding.wordEditText.text.toString()
+            presenter?.getContent(word)
+        }
+
+        binding.wordEditText.setOnFocusChangeListener { view, hasFocus ->
+            if(hasFocus) { showSearch() }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter?.onDetach(this)
+    }
+
+    override fun showSuccess(
+        definitions: StringBuilder,
+        synonyms: String) {
+        binding.definitionsTextview.visibility = View.VISIBLE
+        binding.synonymsTextview.visibility = View.VISIBLE
+        binding.definitionsTextview.text = definitions
+        binding.synonymsTextview.text = synonyms
+    }
+
+    override fun showError() {
+        binding.definitionsTextview.visibility = View.VISIBLE
+        binding.definitionsTextview.text = ERROR_MESSAGE
+    }
+
+    override fun showSearch() {
+        binding.definitionsTextview.visibility = View.GONE
+        binding.synonymsTextview.visibility = View.GONE
     }
 }

bd652b720a75290d71a5b505b4af2b10906511fb
Denis Leonskii <72574009+leonskyd@users.noreply.github.com>
2022-06-06 19:26:35 +0300

    Delete AppState.kt

--- Wordy/app/src/main/java/com/example/wordy/AppState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.wordy
-
-sealed class AppState {
-    data class Success(val data: WordData?) : AppState()
-    data class Error(val error: Throwable) : AppState()
-    data class Loading(val progress: Int?) : AppState()
-}
